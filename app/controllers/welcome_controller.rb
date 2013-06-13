8719ef054bc5e87d63d85b7c6d6126f7a2e182eb
Gabriel Rotbart <gabe@rotbart.net>
2013-06-13 23:40:35 +1000

    first go at getting the bounties on the welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
 
   def index
-    @users = Queries::RunningLeaderboard.new.query.decorate
+    @users           = Queries::RunningLeaderboard.new.query.decorate
+    @latest_bounties = latest_bounties(3)
+  end
+
+  private
+  def latest_bounties(limit_no)
+    Bounty.unclaimed.last(limit_no)
   end
 end
